d96813a0e82119f2322247ef6e686f86b399acb4
Iain Tidbury <iain@mainpages.online>
2023-02-08 20:56:28 +0000

    adds property and booking model validations

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,2 +1,11 @@
 class Property < ApplicationRecord
+  has_one_attached :photo
+  has_many :bookings
+  belongs_to :user
+
+  mount_uploader :photo, PhotoUploader
+
+  validates :title, presence: true
+  validates :location, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
