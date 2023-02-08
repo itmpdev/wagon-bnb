7597178265ade9f7c55220033ea7344159c5ed52
Iain Tidbury <iain@mainpages.online>
2023-02-08 20:26:51 +0000

    adds ingnore for env

--- Gemfile
+++ Gemfile
@@ -48,6 +48,9 @@ gem "sassc-rails"
 # Devise Gem
 gem 'devise'
 
+# Gemfile
+gem "cloudinary"
+
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
---
+++ app/controllers/properties_controller.rb
@@ -0,0 +1,49 @@
+class PropertiesController < ApplicationController
+  before_action :set_property, only: %i[show edit update destroy]
+
+  def index
+    @properties = Property.all
+  end
+
+  def show
+  end
+
+  def new
+    @property = Property.new
+  end
+
+  def create
+    @property = Property.new(property_params)
+    if @property.save
+      redirect_to @property, notice: 'Property was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @property.update(property_params)
+      redirect_to @property, notice: 'Property was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @property.destroy
+    redirect_to properties_url, notice: 'Property was successfully destroyed.'
+  end
+
+  private
+
+  def set_property
+    @property = Property.find(params[:id])
+  end
+
+  def property_params
+    params.require(:property).permit(:name, :description, :address, :price, :user_id)
+  end
+end
